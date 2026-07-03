b8ed07bcf5d546e7b2802b4541bce64a4ad8ad92
mrcat88vthp <mrcat88vthp@gmail.com>
2026-07-03 17:36:44 +0700

    20260703

--- Upload-App/src/infrastructure/websocket/SocketIOGateway.ts
+++ Upload-App/src/infrastructure/websocket/SocketIOGateway.ts
@@ -1,2 +1,24 @@
 import { Server } from "socket.io";
+import type { ISocketGateway } from "@/domain/gateway/ISocketGateway.js";
 
+export class SocketIOGateway implements ISocketGateway {
+    constructor(private readonly io: Server) {
+        // Initialize the Socket.IO server here if needed
+    }
+    
+    async emitToClient (
+        socketId: string,
+        eventName: string,        
+        data: unknown
+    ): Promise<void> {
+        return new Promise((resolve, reject) => {
+            try {
+                const io = new Server();
+                io.to(socketId).emit(eventName, data);
+                resolve();
+            } catch (error) {
+                reject(error);
+            }   
+        });
+    }
+}
